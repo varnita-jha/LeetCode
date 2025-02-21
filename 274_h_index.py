b530cb801aa1bd28d4b7be08cb823a590f0d7d9f
Varnita Jha <39858796+varnita-jha@users.noreply.github.com>
2025-02-21 13:56:23 +0530

    Create 274_h_index.py

---
+++ 274_h_index.py
@@ -0,0 +1,42 @@
+'''
+
+274. H-Index
+
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+
+ 
+
+Example 1:
+
+Input: citations = [3,0,6,1,5]
+Output: 3
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+Example 2:
+
+Input: citations = [1,3,1]
+Output: 1
+ 
+
+Constraints:
+
+n == citations.length
+1 <= n <= 5000
+0 <= citations[i] <= 1000
+
+'''
+
+# Solution
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        hIndex = 0
+        cit = sorted(citations, reverse=True)
+        for i in range(len(cit)):
+            if cit[i] > hIndex and cit[i] <= i + 1:
+                hIndex = cit[i]
+                break
+            hIndex = i + 1 if cit[i] >= i+1 else hIndex
+        return(hIndex)
